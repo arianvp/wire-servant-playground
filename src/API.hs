415d6b2472c5c837c80fda4517299a5c5857cc5f
Arian van Putten <arian.vanputten@gmail.com>
2019-07-12 16:45:04 +0200

    wip

--- src/API.hs
+++ src/API.hs
@@ -3,19 +3,27 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DerivingStrategies #-}
 module API where
 
 import Servant hiding (Unauthorized)
 import Servant.API.Generic
 import Servant.Checked.Exceptions
+import Data.Aeson.Types
 
 
-data ErrorType = NotFound | Unauthorized
 
-data Error (a :: ErrorType) = Error { _label :: String, _Body :: String }
+data NotFound = NotFound 
+  deriving stock (Generic)
+  deriving anyclass (ToJSON, FromJSON)
+
+data Unauthorized = Unauthorized 
+  deriving stock (Generic)
+  deriving anyclass (ToJSON, FromJSON)
 
 data Routes route = Routes
-  { _get :: route :- Capture "id" Int :> Throws (Error 'NotFound) :> Throws (Error 'Unauthorized) :> Get '[JSON] String
+  { _get :: route :- Capture "id" Int :> Throws NotFound :> Throws Unauthorized :> Get '[JSON] String
   , _put :: route :- ReqBody '[JSON] Int :> Put '[JSON] Bool
   }
   deriving (Generic)
--- src/Server.hs
+++ src/Server.hs
@@ -2,10 +2,11 @@ module Server where
 import API
 import Servant.Server.Generic
 import Servant.Server
+import Servant.Checked.Exceptions
 
 server :: Routes AsServer
 server = Routes
-  { _get = const _
+  { _get = const (pureSuccEnvelope "yo")
   , _put = const (return False)
   }
 
